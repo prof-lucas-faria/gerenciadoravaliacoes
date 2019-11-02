4b4d83338b70c88cdafaf5f6fe14871f61333419
Marcos Lima <marcosanthony21@hotmail.com>
2019-11-01 21:01:40 -0300

    Criada InterfaceDAO e Interface Entidade

---
+++ siaproDesktop/src/siapro/dao/InterfaceDAO.java
@@ -0,0 +1,17 @@
+package siapro.dao;
+
+import java.util.List;
+
+import siapro.model.Entidade;
+
+public interface InterfaceDAO {
+	
+public Entidade salvar(Entidade entidade);
+	
+	public Entidade editar(Entidade entidade);
+	
+	public List<Entidade> listarTudo();
+	
+	public Entidade pesquisarId(int id);
+
+}

56d60d60a6013dc49e8a74fcd3c8334c2cdb6b1c
Gustavo Faquim <gustavofaquim408@gmail.com>
2019-11-03 07:36:17 -0300

    Adicionado link dos protótipos.

---
+++ siaproDesktop/src/siapro/dao/AvaliadorDAO.java
@@ -0,0 +1,36 @@
+package siapro.dao;
+
+import java.util.List;
+
+import siapro.model.Entidade;
+
+public class AvaliadorDAO implements InterfaceDAO {
+	private String nome;
+	private String email;
+	private String senha;
+	
+	@Override
+	public Entidade salvar(Entidade entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entidade editar(Entidade entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Entidade> listarTudo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entidade pesquisarId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

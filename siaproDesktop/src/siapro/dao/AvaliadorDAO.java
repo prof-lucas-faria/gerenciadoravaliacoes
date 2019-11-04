8afc71000da53f28656ec8e0a44b90e6b6fb53a6
Gustavo Faquim <gustavofaquim408@gmail.com>
2019-11-04 12:35:53 -0300

    Método salvar feito!

--- siaproDesktop/src/siapro/dao/AvaliadorDAO.java
+++ siaproDesktop/src/siapro/dao/AvaliadorDAO.java
@@ -1,23 +1,42 @@
 package siapro.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import siapro.conexao.Conexao;
+import siapro.model.Avaliador;
 import siapro.model.Entidade;
 
 public class AvaliadorDAO implements InterfaceDAO {
-	private String nome;
-	private String email;
-	private String senha;
+	
+	private Connection conexao;
+	private PreparedStatement stmt;
+	
+	public AvaliadorDAO() {
+		this.conexao = new Conexao().getConexao();
+	}
 	
 	@Override
 	public Entidade salvar(Entidade entidade) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "inser into avaliador(nome,login,senha) values (?,?,?)";
+		Avaliador avaliador = (Avaliador) entidade;
+
+		try {
+			stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, avaliador.getNome());
+			stmt.setString(2,avaliador.getLogin());
+			stmt.setString(3, avaliador.getSenha());
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return null ;
 	}
 
 	@Override
 	public Entidade editar(Entidade entidade) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 

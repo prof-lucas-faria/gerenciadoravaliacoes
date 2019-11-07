438707e5e14084beadc30ae4bab9ae50c4e5b9f3
Hiago Foppa Strach <hiagofoppajager@hotmail.com>
2019-11-07 19:28:24 -0300

    Create CategoriaDAO.java

---
+++ siaproDesktop/src/siapro/dao/CategoriaDAO.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package siapro.dao;
+
+import java.util.List;
+
+import siapro.model.Entidade;
+
+/**
+ * @author HIAGO FOPPA STRACH
+ *
+ */
+public class CategoriaDAO implements InterfaceDAO {
+
+	/**
+	 * 
+	 */
+	public CategoriaDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Entidade salvar(Entidade entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entidade editar(Entidade entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Entidade> listarTudo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entidade pesquisarId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

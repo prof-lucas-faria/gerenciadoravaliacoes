54150d066dcfac55b3124e5094cc2c75621dfb96
Edson Rodrigues <mocosinn@gmail.com>
2019-11-07 21:47:31 -0300

    Arrumei AvaliacaoDAO e Avaliacao(Ainda sem testes)

--- siaproDesktop/src/siapro/dao/AvaliacaoDAO.java
+++ siaproDesktop/src/siapro/dao/AvaliacaoDAO.java
@@ -2,10 +2,14 @@ package siapro.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import siapro.conexao.Conexao;
 import siapro.model.Entidade;
+import siapro.model.Projeto;
+import siapro.model.Avaliacao;
 
 public class AvaliacaoDAO implements InterfaceDAO {
 	 private Connection conexao;
@@ -13,56 +17,61 @@ public class AvaliacaoDAO implements InterfaceDAO {
 	 
 	 public AvaliacaoDAO() {
 		// TODO Auto-generated constructor stub
-		this.conexao = new Conexao().getConexao();
+		this.conexao = new Conexao().getConexao();				
 	}
 
 	@Override
 	public Entidade salvar(Entidade entidade) {
 		// TODO Auto-generated method stub
 		String sql = "INSERT INTO avaliacao (idAvaliador, idProjeto, nota, avaliado) VALUES (?,?,?,?)";
+		Avaliacao avaliacao = (Avaliacao) entidade;
         try {
             stmt = conexao.prepareStatement(sql);
-            stmt.setInt (1, entidade.getAvaliador().getIdAvaliador());
-            stmt.setInt(2, entidade.getProjeto().getIdProjeto());
-            stmt.setDouble(3, entidade.getNota());
-            stmt.setBoolean(4, entidade.getAvaliado());
+            stmt.setLong(1, avaliacao.getAvaliador().getId());
+            stmt.setLong(2, avaliacao.getProjeto().getId());
+            stmt.setDouble(3, avaliacao.getNota());
+            stmt.setBoolean(4, avaliacao.isAvaliacao());
             stmt.execute();
             stmt.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }	
+        }
+		return avaliacao;	
 	}
 
 	@Override
 	public Entidade editar(Entidade entidade) {
 		// TODO Auto-generated method stub
 		String sql = "UPDATE avaliacao SET idAvaliador = ? WHERE idProjeto = ?";
+		Avaliacao avaliacao = (Avaliacao) entidade;
 		try {
 			stmt = conexao.prepareStatement(sql);
-			stmt.setInt (1, entidade.getAvaliador().getIdAvaliador());
-            stmt.setInt(2, entidade.getProjeto().getIdProjeto());
+			stmt.setLong(1, avaliacao.getAvaliador().getId());
+            stmt.setLong(2, avaliacao.getProjeto().getId());
 			stmt.execute();
 			stmt.close();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+		return avaliacao;
 	}
 
 	@Override
-	public List<Entidade> listarTudo() {
+	public List<Entidade> listarTudo(Entidade entidade) {
 		// TODO Auto-generated method stub
 		String sql = "SELECT * FROM avaliacao";
+		
 		try {
 			stmt = conexao.prepareStatement(sql);
 			ResultSet rs = stmt.executeQuery();
-			ArrayList<Avaliacao> lista = new ArrayList<Avaliacao>();
+			ArrayList<Entidade> lista = new ArrayList<Entidade>();
 			while (rs.next()) {
 				Avaliacao avaliacao = new Avaliacao();
-				avaliacao.setIdAvaliacao(rs.getInt("id"));
-				avaliacao.setIdAvaliador(rs.getInt("idAvaliador"));
-				avaliacao.setIdProjeto(rs.getInt("idProjeto"));
+				avaliacao.setId(rs.getLong("id"));
+				avaliacao.getAvaliador().setId(rs.getLong("idAvaliador"));
+				avaliacao.getProjeto().setId(rs.getInt("idProjeto"));
 				avaliacao.setNota(rs.getDouble("nota"));
-				avaliacao.setAvaliado(rs.getBoolean("avaliado"));
+				avaliacao.setAvaliacao(rs.getBoolean("avaliado"));
 				lista.add(avaliacao);
 			}
 			stmt.close();
@@ -73,17 +82,18 @@ public class AvaliacaoDAO implements InterfaceDAO {
 	}
 
 	@Override
-	public Entidade pesquisarId(int id) {
+	public Entidade pesquisarId(long id) {
 		// TODO Auto-generated method stub
 		String sql = "SELECT * FROM avaliacao WHERE id = ?";
+		
 		try {
 			stmt = conexao.prepareStatement(sql);
-			stmt.setInt(1, idAvaliacao);
+			stmt.setLong(1, id);
 			ResultSet rs = stmt.executeQuery();
 			Avaliacao avaliacao = new Avaliacao();
 			if (rs.next()) {
-				avaliacao.setIdAvaliacao(rs.getInt("id"));
-				avaliacao.setAvaliado(rs.getBoolean("avaliado"));
+				avaliacao.setId(rs.getInt("id"));
+				avaliacao.setAvaliacao(rs.getBoolean("avaliado"));
 			}
 			return avaliacao;
 		} catch (Exception e) {

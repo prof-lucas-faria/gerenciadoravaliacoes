b78ab22c44991eb440bf83729df28540bec65516
Ricardo Takayuki Tadokoro <ricardo.tadokoro@ifgoiano.edu.br>
2019-11-08 22:12:40 -0300

    Criação interface gráfica Listar Area

---
+++ siaproDesktop/src/siapro/gui/ListarArea.java
@@ -0,0 +1,52 @@
+package siapro.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JList;
+
+public class ListarArea extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListarArea frame = new ListarArea();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListarArea() {
+		setTitle("Listar Área");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnAdcionarArea = new JButton("Adcionar");
+		contentPane.add(btnAdcionarArea, BorderLayout.NORTH);
+		
+		JList listArea = new JList();
+		listArea.setToolTipText("");
+		contentPane.add(listArea, BorderLayout.WEST);
+	}
+
+}

7a37a8b9b8267425f5c360ab662387711f96a92b
Gustavo Faquim <gustavofaquim408@gmail.com>
2019-11-12 13:56:53 -0300

    Tela Avaliador em construção.

--- siaproDesktop/src/siapro/gui/TelaAvaliador.java
+++ siaproDesktop/src/siapro/gui/TelaAvaliador.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaAvaliador extends JFrame {
 
@@ -35,8 +38,16 @@ public class TelaAvaliador extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("Adicionar Avaliadores");
+		btnNewButton.setBounds(12, 12, 90, 79);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		contentPane.setLayout(null);
+		contentPane.add(btnNewButton);
 	}
 
 }

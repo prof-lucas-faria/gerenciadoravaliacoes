fe0b436517ccca26829c27da352bfccb8654ec1d
Ricardo Takayuki Tadokoro <ricardo.tadokoro@ifgoiano.edu.br>
2019-11-14 21:44:57 -0300

    Atualização II AddProjeto Controller

--- siaproDesktop/src/siapro/controller/AddProjetoController.java
+++ siaproDesktop/src/siapro/controller/AddProjetoController.java
@@ -4,12 +4,16 @@ import siapro.dao.ProjetoDAO;
 import siapro.model.Projeto;
 
 public class AddProjetoController {
+	
+	
 
-	public void salvarProjeto(String titulo,String autores, String categoria, String area) {
+	public void salvarProjeto(String titulo,String autores, Categoria categoria, Area area) {
 		Projeto proj = new Projeto();
 		proj.setTitulo(titulo);
 		proj.setAutores(autores);
-		
+		proj.setCategoria(categoria);
+	
+	
 		
 		new ProjetoDAO().salvar(proj);
 	}

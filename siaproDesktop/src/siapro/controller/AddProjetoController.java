d5297e440cbd5723e9234e63daa8228f23b6a8f8
João Cláudio <jclaudio.caze@gmail.com>
2019-11-26 00:42:52 -0300

    Alterando controller Projeto
    Adicionando alterações ao controller

--- siaproDesktop/src/siapro/controller/AddProjetoController.java
+++ siaproDesktop/src/siapro/controller/AddProjetoController.java
@@ -1,30 +1,45 @@
 package siapro.controller;
 
 import java.awt.geom.Area;
+import java.util.ArrayList;
+import java.util.List;
 
 import siapro.dao.AreaDAO;
 import siapro.dao.CategoriaDAO;
 import siapro.dao.ProjetoDAO;
 import siapro.model.Categoria;
+import siapro.model.Entidade;
 import siapro.model.Projeto;
 
 public class AddProjetoController {
-
-
-	public void salvarProjeto(String titulo,String autores, Categoria categoria, Area area) {
+	
+	public List<Categoria> listarCategoria(Entidade e) {
+		List<Categoria> a = new ArrayList<Categoria>();
+		List<Entidade>	en =	new CategoriaDAO().listarTudo(e);
+		for (Entidade entidade : en) {
+			a.add((Categoria)entidade);
+		}
+		return a;
+	}
+	public List<Area> listarAreas(Entidade e) {
+		List<Area> b = new ArrayList<Area>();
+		List<Entidade>	en =	new AreaDAO().listarTudo(e);
+		for (Entidade entidade : en) {
+			b.add((Area)entidade);
+		}
+		return b;
+	}
+	public void salvarProjeto(String titulo,String autores, Categoria categoria, Area area) {		
+		//CategoriaDAO catdao = new CategoriaDAO();
 		
-		CategoriaDAO catdao = new CategoriaDAO();
-		
-		AreaDAO ardao = new AreaDAO();
-				
+		//AreaDAO ardao = new AreaDAO();				
 		Projeto proj = new Projeto();
 		proj.setTitulo(titulo);
 		proj.setAutores(autores);
-		proj.setCategoria(catdao.pesquisarPorEvento(categoria));
-		proj.setArea(ardao.pesquisarporEvento(area));
-	
-		
+		//List<Categoria> a = new ArrayList<Categoria>();
+		//List<Area> b = new ArrayList<Area>();		
+		//proj.setCategoria(catdao.pesquisarPorEvento(categoria));
+		//proj.setArea(ardao.pesquisarporEvento(area));		
 		new ProjetoDAO().salvar(proj);
 	}
-
 }

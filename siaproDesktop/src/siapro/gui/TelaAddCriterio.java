12537a44bcf7ce2e317d35e27815e1b6329aeba0
Marcos Winicios <pmarcoswinicios@gmail.com>
2019-11-28 19:45:22 -0300

    Removido EXIT_ON_CLOSE da tela add Criteria

--- siaproDesktop/src/siapro/gui/TelaAddCriterio.java
+++ siaproDesktop/src/siapro/gui/TelaAddCriterio.java
@@ -68,7 +68,7 @@ public class TelaAddCriterio extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaAddCriterio() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 528, 343);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

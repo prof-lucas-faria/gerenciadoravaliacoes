dab70e37a9a8f2b18c8b59e324514adf56d3a7d6
Edson Rodrigues <mocosinn@gmail.com>
2019-11-28 20:33:34 -0300

    Exit retirado

--- siaproDesktop/src/siapro/gui/TelaEvento.java
+++ siaproDesktop/src/siapro/gui/TelaEvento.java
@@ -37,7 +37,6 @@ public class TelaEvento extends JFrame {
 //	}
 	public TelaEvento(Evento e) {
 		setTitle("Evento");
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 795, 483);
 		painel = new JPanel();
 		painel.setBorder(new EmptyBorder(5, 5, 5, 5));

23cd23ad5946ab3e7967cb1affeb0077b1361a45
Edson Rodrigues <mocosinn@gmail.com>
2019-11-28 20:33:00 -0300

    Exit retirado

--- siaproDesktop/src/siapro/gui/TelaCategoria.java
+++ siaproDesktop/src/siapro/gui/TelaCategoria.java
@@ -49,7 +49,6 @@ public class TelaCategoria extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaCategoria() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 416, 259);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

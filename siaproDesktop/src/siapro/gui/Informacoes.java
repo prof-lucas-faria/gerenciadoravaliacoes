648e2a6a3cfd6e77b4d8f913255452f30ffd8550
Edson Rodrigues <mocosinn@gmail.com>
2019-11-28 20:23:17 -0300

    Exit retirado

--- siaproDesktop/src/siapro/gui/Informacoes.java
+++ siaproDesktop/src/siapro/gui/Informacoes.java
@@ -31,7 +31,7 @@ public class Informacoes extends JFrame {
 	 * Create the frame.
 	 */
 	public Informacoes() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

494b0ebbc4555e001c5adde6ace30be8b6f8c753
Edson Rodrigues <mocosinn@gmail.com>
2019-11-28 20:32:18 -0300

    Exit retirado

--- siaproDesktop/src/siapro/gui/TelaAvaliador.java
+++ siaproDesktop/src/siapro/gui/TelaAvaliador.java
@@ -53,7 +53,6 @@ public class TelaAvaliador extends JFrame {
 	 * @param e 
 	 */
 	public TelaAvaliador(Evento e) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

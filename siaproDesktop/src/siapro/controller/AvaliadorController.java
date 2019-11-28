9ffd2ed0c88c1aa22dfb209ffdc2b41e72d8b5ce
Gustavo Faquim <gustavofaquim408@gmail.com>
2019-11-28 20:53:21 -0300

    Alterações AvaliadorController

--- siaproDesktop/src/siapro/controller/AvaliadorController.java
+++ siaproDesktop/src/siapro/controller/AvaliadorController.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import siapro.dao.AvaliadorDAO;
+import siapro.dao.CategoriaDAO;
 import siapro.model.Avaliador;
+import siapro.model.Categoria;
 import siapro.model.Entidade;
+import siapro.model.Evento;
 
 public class AvaliadorController {
 	
 	public List<Avaliador> listarAvaliadores(Entidade e) {
 		List<Avaliador> a = new ArrayList<Avaliador>();
-		List<Entidade>	en =	new AvaliadorDAO().listarTudo(e);
+		List<Entidade>	en = new AvaliadorDAO().listarTudo(e);
 		for (Entidade entidade : en) {
 			a.add((Avaliador)entidade);
 		}
+			
 		return a;
 	}
 

c34129d0f9cb495f7f829264192131a702a4d499
Edson Rodrigues <mocosinn@gmail.com>
2019-11-28 20:31:44 -0300

    Exit retirado

--- siaproDesktop/src/siapro/gui/TelaAddProjeto.java
+++ siaproDesktop/src/siapro/gui/TelaAddProjeto.java
@@ -58,7 +58,6 @@ public class TelaAddProjeto extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaAddProjeto(Evento evento) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 910, 542);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));

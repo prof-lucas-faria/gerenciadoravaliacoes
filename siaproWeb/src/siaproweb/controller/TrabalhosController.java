f08e9fc912ac5d64dfce1ecaec8c3b16c1c6ccc0
Hiago Foppa Strach <hiagofoppajager@hotmail.com>
2019-12-06 21:48:30 -0300

    Controler e tela Listar Trabalhos
    Add Managed Bean e organização

--- siaproWeb/src/siaproweb/controller/TrabalhosController.java
+++ siaproWeb/src/siaproweb/controller/TrabalhosController.java
@@ -2,11 +2,14 @@ package siaproweb.controller;
 
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+
 import siaproweb.dao.ProjetoDAO;
 import siaproweb.model.Avaliador;
 import siaproweb.model.Evento;
 import siaproweb.model.Projeto;
 
+@ManagedBean
 public class TrabalhosController {
 	
 	public List<Projeto> listarTrabalhosAvaliador(Evento e,Avaliador a ) {

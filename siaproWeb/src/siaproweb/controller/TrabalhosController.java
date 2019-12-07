92712ba463206a88a01342a343d8738e28470e33
Hiago Foppa Strach <hiagofoppajager@hotmail.com>
2019-12-06 22:00:34 -0300

    ManagedBean
    Organizando

--- siaproWeb/src/siaproweb/controller/TrabalhosController.java
+++ siaproWeb/src/siaproweb/controller/TrabalhosController.java
@@ -3,13 +3,14 @@ package siaproweb.controller;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 import siaproweb.dao.ProjetoDAO;
 import siaproweb.model.Avaliador;
 import siaproweb.model.Evento;
 import siaproweb.model.Projeto;
 
-@ManagedBean
+@ManagedBean 
 public class TrabalhosController {
 	
 	public List<Projeto> listarTrabalhosAvaliador(Evento e,Avaliador a ) {

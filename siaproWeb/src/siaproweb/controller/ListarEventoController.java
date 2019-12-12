c1f2f01208dad78cd04cf221aa976f11b9a4ea03
João Cláudio <jclaudio.caze@gmail.com>
2019-12-11 22:42:21 -0300

    Listando Evento web
    Com ajuda do Marcos, pagina listando os eventos de acordo com o o avaliador (session)

--- siaproWeb/src/siaproweb/controller/ListarEventoController.java
+++ siaproWeb/src/siaproweb/controller/ListarEventoController.java
@@ -1,18 +1,23 @@
 package siaproweb.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import siaproweb.core.SessionContext;
+import siaproweb.dao.EventoDAO;
 import siaproweb.model.Avaliador;
+import siaproweb.model.Entidade;
+import siaproweb.model.Evento;
 import siaproweb.model.Projeto;
 
 @ManagedBean
 @RequestScoped
 
 public class ListarEventoController {
-	private Avaliador avaliador;
-	private Projeto projeto;
+	private Avaliador avaliador = ((Avaliador) SessionContext.getInstance().getAttribute("userObject"));
 	
 	public Avaliador getAvaliador() {
 		return avaliador;
@@ -22,15 +27,16 @@ public class ListarEventoController {
 		this.avaliador = avaliador;
 	}
 	
-	public ListarEventoController(){
-		this.avaliador = (Avaliador) SessionContext.getInstance().getAttribute("userObject");
+	public ArrayList <Evento> listarEventos(){
+		List<Entidade> entidades = new EventoDAO().listarTudo(this.avaliador);
+		ArrayList<Evento> eventos = new ArrayList <Evento>();
+		for (Entidade entidade : entidades) {
+			eventos.add((Evento) entidade);
+		}
+		return eventos;
 	}
 	
 	public Projeto getProjeto() {	
 		return getProjeto();
-	}
-
-	public void setPprojeto(Projeto proj) {
-		this.projeto = proj;
 	}
 }
